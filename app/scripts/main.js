7d011ac274a848c83627bfb1a57211fdfb211f62
Ken Peltzer <kpeltzer@gmail.com>
2015-04-12 19:40:37 -0400

    More work

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,13 +5,19 @@ var rtd = (function() {
     var tokenObject, 
         accessToken,
         selectedVertical,
+        location = {
+            lat: 0,
+            lng: 0
+        },
         $uberPanelBody = $('.uber-panel-body'),
         $uberConnectedCheck = $('.uber-check'),
         $eatOrDrinkCheck = $('.eat-or-drink-check'),
         $uberButton = $('.uber-button > img'),
+        $addressButton = $('.address-button'),
+        $verticalButton = $('.vertical-button'),
         $rollButton = $('.roll-button'),
 
-        //Variable to hold state
+        //Variable to hold state of steps
         formState = {
             uber: false,
             address: false,
@@ -26,6 +32,25 @@ var rtd = (function() {
             scopes: { request: ["request"]}
         }),
 
+        init = function() {
+            //Check oauth params
+            jso.callback();
+
+            //Check if token exists
+            tokenObject = jso.checkToken();
+
+            //Check if Uber token already exists
+            if (tokenObject) {
+                accessToken = tokenObject.access_token;
+                $uberConnectedCheck.show();
+                state.uber = true;
+                checkIfRequestReady();
+            }
+            else {
+                $uberPanelBody.show();
+            }
+        }
+
         checkIfRequestReady = function() {
             if (
                 state.uber &&
@@ -36,36 +61,69 @@ var rtd = (function() {
             }
         },
 
+        convertCoordinatesToAddress = function(lat, lng, cb) {
+            $.ajax({
+                url: 'http://maps.googleapis.com/maps/api/geocode/json',
+                data: {
+                    'latlng': lat + ',' + lng
+                },
+                dataType: 'json'
+            })
+            .done(function(data) {
+                if (data.results && data.results.length && typeof cb === 'function') {
+                    formState.address = true;
+                    cb(data.results[0].formatted_address);
+                    checkIfRequestReady();
+
+                }
+            })
+        }
+
         requestSolr = function(vertical) {
             var promise = $.ajax({
-                url: 'https'
+                
             })
         };
 
-    //Check oauth params
-    jso.callback();
-
-    //Check if token exists
-    tokenObject = jso.checkToken();
-
-    if (tokenObject) {
-        accessToken = tokenObject.access_token;
-        $uberConnectedCheck.show();
-        state.uber = true;
-        checkIfRequestReady();
-    }
-    else {
-        $uberPanelBody.show();
-    }
+    
 
     //Click handlers
     $uberButton.click(function(e){
         jso.getToken();
     });
 
+    $addressButton.click(function(e) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+
+                if (position) {
+                    location.lat = position.coords.latitude;
+                    location.lng = position.coords.longitude;
+
+                    //Convert coordinates to lat lng
+                    convertCoordinatesToAddress(location.lat, location.lng, function(address) {
+                        $addressButton.hide();
+                        $('.address-text').text(address);
+                        $('.address-check .check').show();
+                    })
+                }
+            });
+        }
+    });
+
+    $verticalButton.click(function(e) {
+        selectedVertical = $(this).data('vertical'));
+    })
+
     $rollButton.click(function(e) {
         $rollButton.button('loading');
+
+        if (selectedVertical) {
+
+        }
     });
+
+    init();
 
 
 })();
