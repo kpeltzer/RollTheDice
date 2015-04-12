77a5b935edfb5dde0ae50331ce6b49ed80981da5
Ken Peltzer <ken@thrillist.com>
2015-04-12 17:26:57 -0400

    More styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,73 @@
 /* jshint devel:true */
 
-var jso = new JSO({
-    providerID: "uber",
-    client_id: "YAgrpKavtUpQU_jXyrZXMny3gLwWTxsF",
-    redirect_uri: "http://localhost:9000",
-    authorization: "https://login.uber.com/oauth/authorize",
-    scopes: { request: ["request"]}
-});
+var rtd = (function() {
 
-//Check oauth params
-jso.callback();
+    var tokenObject, 
+        accessToken,
+        selectedVertical,
+        $uberPanelBody = $('.uber-panel-body'),
+        $uberConnectedCheck = $('.uber-check'),
+        $eatOrDrinkCheck = $('.eat-or-drink-check'),
+        $uberButton = $('.uber-button > img'),
+        $rollButton = $('.roll-button'),
 
-$('.uber-button').click(function(e){
-    jso.getToken();
-});
+        //Variable to hold state
+        formState = {
+            uber: false,
+            address: false,
+            vertical: false
+        },
+
+        jso = new JSO({
+            providerID: "uber",
+            client_id: "YAgrpKavtUpQU_jXyrZXMny3gLwWTxsF",
+            redirect_uri: "http://localhost:9000",
+            authorization: "https://login.uber.com/oauth/authorize",
+            scopes: { request: ["request"]}
+        }),
+
+        checkIfRequestReady = function() {
+            if (
+                state.uber &&
+                state.address &&
+                state.vertical
+            ) {
+                $rollButton.show();
+            }
+        },
+
+        requestSolr = function(vertical) {
+            var promise = $.ajax({
+                url: 'https'
+            })
+        };
+
+    //Check oauth params
+    jso.callback();
+
+    //Check if token exists
+    tokenObject = jso.checkToken();
+
+    if (tokenObject) {
+        accessToken = tokenObject.access_token;
+        $uberConnectedCheck.show();
+        state.uber = true;
+        checkIfRequestReady();
+    }
+    else {
+        $uberPanelBody.show();
+    }
+
+    //Click handlers
+    $uberButton.click(function(e){
+        jso.getToken();
+    });
+
+    $rollButton.click(function(e) {
+        $rollButton.button('loading');
+    });
+
+
+})();
+
+
